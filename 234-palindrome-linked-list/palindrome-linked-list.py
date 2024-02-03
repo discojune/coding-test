edbf11be02bb489415966614fda62d991ce9c646
discojune <152982386+discojune@users.noreply.github.com>
2024-02-03 12:18:55 +0900

    Time: 268 ms (97.83%) | Memory: 27.3 MB (99.00%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,22 +5,22 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        q_head: Deque = collections.deque()
+        rev = None
+        slow = fast = head
 
-        if not head:
-            return True
+        # 런너를 이용해 역순 연결 리스트 구성
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next
 
-        node = head
-
-        while node is not None:
-            q_head.append(node.val)
-            node = node.next
-
-        while len(q_head) > 1:
-            if q_head.popleft() != q_head.pop():
-                return False
-
-        return True
-
+        # 연결 리스트가 홀수 개로 연결되어 있는 경우, 중앙을 한번 더 이동한다.
+        if fast:
+            slow = slow.next
             
+        # 팰린드롬 여부 확인
+        # slow 런너는 중앙값으로부터 끝까지 나아가면서 rev와 값을 비교하며 팰린드폼인지를 확인한다.
+        while rev and rev.val == slow.val:
+            slow, rev = slow.next, rev.next # 각 러너의 포인트를 이동한다.
+
+        return not rev
 
