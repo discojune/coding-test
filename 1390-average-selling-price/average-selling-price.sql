799da166bf2ca8e1c7cff9594bef07d5a97dbd9b
discojune <152982386+discojune@users.noreply.github.com>
2024-02-20 15:43:40 +0900

    Time: 1408 ms (55.63%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
+-- join이 되지 않는 경우에 대한 예외처리 필요
 select p.product_id, ifnull(round(sum(p.price * u.units) / sum(u.units), 2), 0) as average_price
 from Prices as p left join UnitsSold as u
 -- join이 발생하는 조건을 명시
