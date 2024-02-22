b15dcbff01f9c06df22a8286db440945e46a3047
discojune <152982386+discojune@users.noreply.github.com>
2024-02-22 15:12:13 +0900

    Time: 2625 ms (26.33%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,7 +1,26 @@
 # Write your MySQL query statement below
-select (select u.name from movierating m left join users u on m.user_id = u.user_id group by m.user_id 
-order by count(*) DESC,u.name ASC limit 1) as results
+select min(name) as results
+from
+(select name,
+user_rating.rating_counts as rating
+from Users as u
+left join 
+(select user_id, count(rating) as rating_counts from MovieRating group by user_id) as user_rating
+on u.user_id = user_rating.user_id) 
+as temp1
+-- from 으로 사용할 테이블은 alias가 필요하다
+where temp1.rating = (select max(rating_counts) from (select user_id, count(rating) as rating_counts from MovieRating group by user_id) as t)
+-- 중복된 값을 모두 합치는 명령
 union all
-select (select mm.title from movies mm right join movierating m on mm.movie_id = m.movie_id 
-where DATE_FORMAT(m.created_at,'%Y-%m') = '2020-02' group by m.movie_id order by avg(m.rating)
-DESC,mm.title ASC limit 1) as results;
+select min(title) as results
+from
+(select m.movie_id, m.title, avg_mr.avg_rating
+from Movies as m
+left join 
+(select movie_id, avg(rating) as avg_rating
+from MovieRating
+where DATE_FORMAT(created_at,'%Y-%m') = '2020-02'
+group by movie_id) as avg_mr
+on m.movie_id = avg_mr.movie_id) as temp2
+where temp2.avg_rating = (select max(avg_rating) from (select movie_id, avg(rating) as avg_rating from MovieRating where DATE_FORMAT(created_at,'%Y-%m') = '2020-02' group by movie_id) as t)
+
