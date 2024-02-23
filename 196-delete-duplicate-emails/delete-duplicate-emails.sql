a1091311b39985ba178f728dfb39da93e8db7486
discojune <152982386+discojune@users.noreply.github.com>
2024-02-23 16:23:00 +0900

    Time: 2168 ms (11.48%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
+-- 참고: https://leetcode.com/problems/delete-duplicate-emails/solutions/2627589/my-sql-solution
 delete p from Person as p, Person as p1
 where p.email = p1.email and p.id > p1.id
