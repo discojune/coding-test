52e76596285f3738f6662d699e741cba6efb218c
discojune <152982386+discojune@users.noreply.github.com>
2024-06-02 22:40:54 +0900

    Time: 98 ms (5.34%) | Memory: 18.2 MB (41.29%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -0,0 +1,32 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        from collections import deque
+
+        root = 0
+        queue = deque([(graph[root], "0")])
+
+        paths = []
+        while queue:
+            curr_node, path = queue.popleft()
+
+            for next_idx in curr_node:
+                new_path = path + ',' + str(next_idx)
+
+                next_node = graph[next_idx]
+                
+                print(new_path)
+                if not next_node and int(new_path.split(',')[-1]) == len(graph)-1:
+                    
+                    paths.append([int(s) for s in new_path.split(',')])
+                    continue
+
+                if int(new_path.split(',')[-1]) == len(graph)-1:
+                    paths.append([int(s) for s in new_path.split(',')])
+
+                queue.append([next_node, new_path])
+        
+
+        return paths
+
+
+
