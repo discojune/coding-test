204fd796e0271c60994e733d44441b7a33fcb264
discojune <152982386+discojune@users.noreply.github.com>
2024-06-15 10:26:24 +0900

    Time: 139 ms (50.52%) | Memory: 18.5 MB (87.01%) - LeetSync

--- 1572-subrectangle-queries/subrectangle-queries.py
+++ 1572-subrectangle-queries/subrectangle-queries.py
@@ -1,15 +1,14 @@
 class SubrectangleQueries:
-    def __init__(self, rectangle):
-        self.rectangle = rectangle
+    def __init__(self, rectangle: List[List[int]]):
+        self.rect = rectangle
 
-    def updateSubrectangle(self, row1, col1, row2, col2, newValue):
-        for i in range(row1, row2 + 1):
-            for j in range(col1, col2 + 1):
-                self.rectangle[i][j] = newValue
+    def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for i in range(row1, row2+1):
+            for j in range(col1, col2+1):
+                self.rect[i][j] = newValue
 
-    def getValue(self, row, col):
-        return self.rectangle[row][col]
-        
+    def getValue(self, row: int, col: int) -> int:
+        return self.rect[row][col]
 
 
 # Your SubrectangleQueries object will be instantiated and called as such:
