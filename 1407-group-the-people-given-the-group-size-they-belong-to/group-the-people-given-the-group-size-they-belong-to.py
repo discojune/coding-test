fb697d02974c669a73be0818474ecb23a6cf4201
discojune <152982386+discojune@users.noreply.github.com>
2024-06-16 12:20:23 +0900

    Time: 70 ms (29.66%) | Memory: 17 MB (8.45%) - LeetSync

---
+++ 1407-group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        from collections import deque
+
+        index_group = [i for i in range(0, len(groupSizes))]
+        index_group = [(j, i) for j, i in sorted(zip(groupSizes, index_group))]
+        
+        queue = deque(index_group)
+        print(queue)
+        result = []
+        temp = []
+        while queue:
+            group, person = queue.popleft()
+            # print(group, person, temp)
+
+            temp.append(person)
+
+            if len(temp) >= group:
+                result.append(temp)
+                temp = []
+
+        return result
